8f979ac2fb5ed891551ba0f952e632f4afc90797
Татьяна <lee-tanya2insp@yandex.ru>
2024-02-21 08:56:41 +0300

    Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,46 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import PlaceCard from '../place-card';
-
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorite-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PrivateRoute from '../private-route/private-route';
 
 function App(): JSX.Element {
   return (
-    <MainPage>
-      <PlaceCard/>
-      <PlaceCard/>
-      <PlaceCard/>
-      <PlaceCard/>
-      <PlaceCard/>
-    </MainPage>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<MainPage><PlaceCard /> <PlaceCard/> <PlaceCard/> <PlaceCard/> <PlaceCard/></MainPage>}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferPage />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import {PropsWithChildren} from 'react';
+import Logo from '../../components/logo/logo';
 
 function MainPage({ children }: PropsWithChildren): JSX.Element {
   return (
@@ -6,11 +7,8 @@ function MainPage({ children }: PropsWithChildren): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo />
+
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
